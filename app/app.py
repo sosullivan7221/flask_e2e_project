53fd570b9bd75127da6ad77a57a8cc7ed602c811
sosullivan7221 <sosullivan7221@gmail.com>
2023-12-14 23:53:47 +0000

    flask app

---
+++ app/app.py
@@ -0,0 +1,34 @@
+from sqlalchemy import create_engine, inspect, text
+import sqlalchemy
+import os
+from dotenv import load_dotenv 
+from flask import Flask, render_template, request
+
+DB_HOST = os.getenv("DB_HOST")
+DB_DATABASE = os.getenv("DB_DATABASE")
+DB_USERNAME = os.getenv("DB_USERNAME")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+
+# Connection string
+connect_args={'ssl':{'fake_flag_to_enable_tls': True}}
+conn_string = (
+    f"mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}/{DB_DATABASE}")
+engine = create_engine(conn_string)
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+@app.route('/patients')
+def patients():
+    with engine.connect() as connection:
+        query_1 = text('SELECT * FROM patient_info')
+        result_1 = connection.execute(query_1)
+        table_1 = result_1.fetchall()
+
+    return render_template('patients.html', data1=table_1)
+
+if __name__ == '__main__':
+    app.run(debug=True)
